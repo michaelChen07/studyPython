d7c359791a784d08afa1b3bc8888e6d8fad3a69a
michaelChen07 <286522215@qq.com>
2016-11-10 01:11:50 +0800

    find waterflower number

---
+++ cn/wensi/practice/flower.py
@@ -0,0 +1,31 @@
+# encoding=utf-8
+import datetime
+
+def get_size(number):
+    count = 0
+    while(number >= 10):
+        number /= 10
+        count += 1
+        return count + 1
+
+def get_flower(max_int):
+    for number in range(max_int):
+        if(if_flower(number)):
+            print number
+
+
+def if_flower(number):
+    sum = 0
+    temp_number = number
+    count = get_size(temp_number)
+    for j in range(count):
+        mod = temp_number % 10
+        temp_number /= 10
+        sum += pow(mod, count)
+        return sum == number
+
+max_int = raw_input("输入最大数: ")
+start = datetime.datetime.now()
+get_flower(max_int)
+end = datetime.datetime.now()
+print "总共花费时间：" + str((end-start).seconds) + "s"
