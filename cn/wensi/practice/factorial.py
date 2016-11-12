f687e0bef3cd43a5ae4c2faaf7fdbe0a12b2873f
michaelChen07 <286522215@qq.com>
2016-11-12 11:01:31 +0800

    将列表中的元素遍历一遍，但列表中的元组和列表中的元素在一行

---
+++ cn/wensi/practice/factorial.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+t = [1,24,[3,4,5],('a','b','c'), 8, 9]
+for i in range(len(t)):
+    if isinstance(t[i],(list,tuple)):
+        for j in t[i]:
+            print j,#将列表和元组中的元素打印到一行
+        print#换行
+    else:
+        print t[i]
+
