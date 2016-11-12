f145fff1cedc760f87fd21df893a746214bf117e
michaelChen07 <286522215@qq.com>
2016-11-12 14:22:02 +0800

    10-12岁女孩加入足球队

--- cn/wensi/practice/factorial.py
+++ cn/wensi/practice/factorial.py
@@ -1,10 +1,21 @@
 #encoding=utf-8
-t = [1,24,[3,4,5],('a','b','c'), 8, 9]
-for i in range(len(t)):
-    if isinstance(t[i],(list,tuple)):
-        for j in t[i]:
-            print j,#将列表和元组中的元素打印到一行
-        print#换行
+sum_people = 0
+for i in range(10):
+
+    sex = raw_input("please input your sex (f or m):")
+    if sex == "m":
+        print "sorry,we can not play together!"
+        continue
+    elif sex == "f":
+        age = int(raw_input("please input your age:"))
+        if age >=10 and age <= 12:
+            print "welcome to join our football team!"
+            sum_people += 1
+        else:
+            print "sorry!"
     else:
-        print t[i]
+        print "your input sex is wrong , please reinput!"
+        continue
+
+print "our football team has %d members!" %sum_people
 
