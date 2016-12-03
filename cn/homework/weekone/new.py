28c614ab35f4b91dda9b9660c99595c960cd3757
michaelChen07 <286522215@qq.com>
2016-12-03 11:48:55 +0800

    元组，字典，列表基本操作

--- cn/homework/weekone/new.py
+++ cn/homework/weekone/new.py
@@ -1,20 +1,10 @@
 #encoding=utf-8
-a=(1,2,(3,4,5))
-b=(7,8,9)
+a=[2,5,5,8,8]
+a.sort()
+print a[-2]
 
-#拼接
-print a+b
 
-#计算元素个数
-print len(a),len(b)
-
-#复制
-print b*4
-
-#判断元素是否在列表中
-print 2 in a
-
-#迭代，遍历
-for i in b:
-    print i
+b=list(set(a))
+b.sort()
+print b[-2]
 
