25a5869c5916ecb920ca12ed92b5b38a90c8e88b
michaelChen07 <286522215@qq.com>
2016-12-03 11:34:07 +0800

    添加文件

--- cn/homework/weekone/new.py
+++ cn/homework/weekone/new.py
@@ -1,20 +1,6 @@
 #encoding=utf-8
-import copy
-lista=[1,2,3,4,[5,6,7]]
-listb=copy.deepcopy(lista)
-listc=copy.copy(lista)
-
-#改变lista一维的值
-lista[0]=9
-print "lista=",lista
-print "listb=",listb
-print "listc=",listc
-
-#分隔符
-print "_"*30
-
-#改变lista二维的值
-lista[4][0]=8
-print "lista=",lista
-print "listb=",listb
-print "listc=",listc
+a=(1,2,(3,4,5))
+count=0
+for i in a:
+    count +=1
+print count
