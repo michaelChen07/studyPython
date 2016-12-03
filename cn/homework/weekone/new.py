10f5243ad89e3bc1714248eb47dc90cea5ef6673
michaelChen07 <286522215@qq.com>
2016-12-03 10:23:45 +0800

    修改了杨辉三角的代码

--- cn/homework/weekone/new.py
+++ cn/homework/weekone/new.py
@@ -1,5 +1,20 @@
 #encoding=utf-8
-lista = [4,3,6,12,56]
-for i in xrange(1,len(lista)):
-    lista[i] += lista[i-1]
-print lista
+import copy
+lista=[1,2,3,4,[5,6,7]]
+listb=copy.deepcopy(lista)
+listc=copy.copy(lista)
+
+#改变lista一维的值
+lista[0]=9
+print "lista=",lista
+print "listb=",listb
+print "listc=",listc
+
+#分隔符
+print "_"*30
+
+#改变lista二维的值
+lista[4][0]=8
+print "lista=",lista
+print "listb=",listb
+print "listc=",listc
--- cn/homework/weekone/yanghuisanjiao.py
+++ cn/homework/weekone/yanghuisanjiao.py
@@ -1,4 +1,5 @@
 #encoding=utf-8
+#原代码
 def get_yanghui():
     b=[1]
     while 1:
@@ -13,5 +14,25 @@ def print_yanghui(n):
         print "\n"
         a+=1
         if a==n:
+            break
+print_yanghui(10)
+
+
+
+#龙宝宝修改后的
+def get_yanghui(num):
+    b=[1]
+    while 1:
+        yield " " * num + str(b)
+        b = [1] + [b[i] + b[i + 1] for i in range(len(b) - 1)] + [1]
+        num -= 1
+
+def print_yanghui(n):
+    a=0
+    num = n
+    for t in get_yanghui(num):
+        print t + "\n",
+        a += 1
+        if a == n:
             break
 print_yanghui(10)
