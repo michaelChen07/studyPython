6085b75b2b892b9aa3aa0f96d91ea88f53ee5ed1
michaelChen07 <286522215@qq.com>
2016-12-11 23:24:32 +0800

    递归实现列表求和,适合多元列表

--- cn/homework/week4/listSumbyRecursion.py
+++ cn/homework/week4/listSumbyRecursion.py
@@ -1,4 +1,5 @@
 #encoding=utf-8
+
 #递归实现列表求和,适合多元列表
 
 
@@ -8,7 +9,7 @@ def get_sumlist(lista):
     sum = 0
     for i in lista:
         if isinstance(i,(list,tuple)):
-            sum += get_sumlist(i)
+            sum += get_sumlist(i) #多元列表再次调用
         else:
             sum += i
     return sum
