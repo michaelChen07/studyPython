d46f3dc910b2d15adc1ee4ca2f0af48d76fdaf2a
michaelChen07 <286522215@qq.com>
2017-02-18 12:03:22 +0800

    匹配连续5个数字

--- cn/homework/week5/reDemo.py
+++ cn/homework/week5/reDemo.py
@@ -19,8 +19,8 @@ def test_regular(rule,destine_string):
         return None
 
 #测试
-if __name__=="__main__":
-    assert test_regular(rule,"12345")=="12345"
+if __name__=="__main__":#当模块被直接运行时，以下代码块将被运行，当模块是被导入时，代码块不被运行
+    assert test_regular(rule,"12345")=="12345"#断言，正确则继续执行，错误则停止
     assert test_regular(rule,"12345 ")=="12345"
     assert test_regular(rule," 12345")=="12345"
     assert test_regular(rule,"x12345x")=="12345"
